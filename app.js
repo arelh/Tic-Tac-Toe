b03c3841152d208e775a1d7d1a999de9d0360469
arel hagag <areltv297@gmail.com>
2022-10-16 16:18:30 +0300

    dfd

--- app.js
+++ app.js
@@ -3,10 +3,11 @@
 // 3. switch_Turn
 
 const board = document.querySelector(".board");
-const cell = document.querySelectorAll(".cell");
+const cells = document.querySelectorAll(".cell");
 const x = document.createElement("x");
 const o = document.createElement("o");
-
+const h3 = document.querySelector("h3");
+const restart = document.querySelector("#restartButton");
 const WINNING_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -17,6 +18,85 @@ const WINNING_COMBINATIONS = [
   [0, 4, 8],
   [2, 4, 6],
 ];
+
+/*----------check win---------*/
+const calculateWinner = () => {
+  if (
+    (cells[0].textContent === "o" || cells[0].textContent === "x") &&
+    cells[0].textContent == cells[1].textContent &&
+    cells[0].textContent == cells[2].textContent
+  ) {
+    return cells[0].textContent + " Won";
+  } else if (
+    (cells[3].textContent === "o" || cells[3].textContent === "x") &&
+    cells[3].textContent == cells[4].textContent &&
+    cells[3].textContent == cells[5].textContent
+  ) {
+    return cells[3].textContent + " Won";
+  } else if (
+    (cells[6].textContent === "o" || cells[6].textContent === "x") &&
+    cells[6].textContent == cells[7].textContent &&
+    cells[6].textContent == cells[8].textContent
+  ) {
+    return cells[6].textContent + " Won";
+  } else if (
+    (cells[0].textContent === "o" || cells[0].textContent === "x") &&
+    cells[0].textContent == cells[3].textContent &&
+    cells[0].textContent == cells[6].textContent
+  ) {
+    return cells[0].textContent + " Won";
+  } else if (
+    (cells[1].textContent === "o" || cells[1].textContent === "x") &&
+    cells[1].textContent == cells[4].textContent &&
+    cells[1].textContent == cells[7].textContent
+  ) {
+    return cells[1].textContent + " Won";
+  } else if (
+    (cells[2].textContent === "o" || cells[2].textContent === "x") &&
+    cells[2].textContent == cells[5].textContent &&
+    cells[2].textContent == cells[8].textContent
+  ) {
+    return cells[2].textContent + " Won";
+  } else if (
+    (cells[0].textContent === "o" || cells[0].textContent === "x") &&
+    cells[0].textContent == cells[4].textContent &&
+    cells[0].textContent == cells[8].textContent
+  ) {
+    return cells[0].textContent + " Won";
+  } else if (
+    (cells[2].textContent === "o" || cells[2].textContent === "x") &&
+    cells[2].textContent == cells[4].textContent &&
+    cells[2].textContent == cells[6].textContent
+  ) {
+    return cells[2].textContent + " Won";
+  } else if (
+    cells[0].textContent != "" &&
+    cells[1].textContent != "" &&
+    cells[2].textContent != "" &&
+    cells[3].textContent != "" &&
+    cells[4].textContent != "" &&
+    cells[5].textContent != ""
+  ) {
+    return "drew";
+  }
+};
+
+//-------------do if the game end in winner or drew--------
+const clickHandler = (e) => {
+  put(e.target);
+  const winner = calculateWinner();
+  if (winner) {
+    h3.innerText = winner.toUpperCase();
+    console.log(winner); //do something to tell about the winner
+  }
+  if (calculateDraw()) {
+    console.log("drew");
+  }
+};
+
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener("click", clickHandler, { once: true });
+}
 
 const gameState = {
   x: true,
@@ -31,12 +111,10 @@ function put(e) {
     gameState.x = true;
   }
 }
-
-board.addEventListener("click", (e) => {
-  
-  if (e.target.classList.contains("x") || e.target.classList.contains("o")) {
+function reset() {
+  for (let item of cells) {
+    item.textContent = "";
   }
-  return put(e.target);
-});
-
-
+}
+board.addEventListener("click", clickHandler, { once: true });
+restart.addEventListener("click", reset);
